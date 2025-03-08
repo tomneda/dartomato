8360249fc0cecc40782b3a2a717fcc7c8a62c44a
Michael Mildner <m1ld3@gmx.de>
2025-03-08 15:47:23 +0100

    Adaptions for Qt > 6.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "dartomat_mainwindow.h"
 
 #include <QApplication>
+//#include <QStyleFactory>
 
 #ifndef TESTING
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+  a.setStyle("fusion");
   CDartomatMain w;
   w.show();
   return a.exec();
